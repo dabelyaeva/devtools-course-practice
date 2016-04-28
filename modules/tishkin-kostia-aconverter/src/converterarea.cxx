2825e908eade04f881968c023a07361e600ac366
Konstantin Tishkin <1995kostia@mail.ru>
2016-04-28 13:25:30 +0300

    Update src

--- modules/tishkin-kostia-aconverter/src/converterarea.cxx
+++ modules/tishkin-kostia-aconverter/src/converterarea.cxx
@@ -4,9 +4,9 @@
 
 AreaConverter::AreaConverter(const double a, const Type type) {
     if ((a < 0.0) && (type == Type::HECTARE)) {
-		status = false;
+        status = false;
     } else {
-		status = true;
+        status = true;
         area_ = a;
         type_ = type;
         }
@@ -18,34 +18,34 @@ double AreaConverter::convert(const Type type) {
             switch (type_) {
             case Type::HECTARE:
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::WEAVING:
                 if (area_ < 0.0) {
-					status = false;
+                    status = false;
                 } else {
                     area_ = area_* 100;
                     type_ = type;
                     }
-				return area_;
+                return area_;
                 break;
             case Type::METER:
                 if (area_  < 0.0) {
-					status = false;
+                    status = false;
                 } else {
                     area_ = (area_ * 10000);
                     type_ = type;
                     }
-				return area_;
+                return area_;
                 break;
             case Type::CENTIMETER:
                 if (area_ < 0.0) {
-					status = false;
+                    status = false;
                 } else {
                     area_ = area_ * 100000000;
                     type_ = type;
                     }
-				return area_;
+                return area_;
                 break;
             }
         break;
@@ -54,21 +54,21 @@ double AreaConverter::convert(const Type type) {
             case Type::HECTARE:
                 area_ = area_ * 0.01;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::WEAVING:
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::METER:
                 area_ = area_ * 100;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::CENTIMETER:
                 area_ = 10000 * area_;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             }
         break;
@@ -77,21 +77,21 @@ double AreaConverter::convert(const Type type) {
             case Type::HECTARE:
                 area_ = area_ * 0.00001;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::WEAVING:
                 area_ = 0.01 * area_;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::METER:
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::CENTIMETER:
                 area_ = area_ * 100;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             }
         break;
@@ -100,21 +100,21 @@ double AreaConverter::convert(const Type type) {
             case Type::HECTARE:
                 area_ = 0.0000000001 * area_;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::WEAVING:
                 area_ = area_ * 0.0001;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::METER:
                 area_ = area_ * 0.01;
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             case Type::CENTIMETER:
                 type_ = type;
-				return area_;
+                return area_;
                 break;
             }
         break;
@@ -123,7 +123,7 @@ double AreaConverter::convert(const Type type) {
 
 bool AreaConverter::getRetCode() {
     return status;
-	status = true;
+    status = true;
 }
 
 double AreaConverter::getarea_()const {

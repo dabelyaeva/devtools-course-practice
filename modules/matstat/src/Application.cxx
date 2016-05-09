7f7a6519a10999e899f8df05248e897c4937a700
Andrew Marchenko <andrey-95nn@yandex.ru>
2016-05-10 00:08:10 +0300

    Fixed structure

--- modules/matstat/src/Application.cxx
+++ modules/matstat/src/Application.cxx
@@ -1,9 +1,13 @@
 // Copyright 2016 Marchenko Andrey
 
 #include "include/Application.h"
-
-#include <string>
-#include <cmath>
 #include <vector>
 
 using std::vector;
+
+Application::Application() {
+    vector<double> v1 = { 0 };
+    vector<double> v2 = { 1 };
+    Sample s1(v1, v2);
+    s = s1;
+}

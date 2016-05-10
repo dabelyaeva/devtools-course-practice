47f093153ac1be9b30e28ec887ef573d97040b84
cetygamer <cetygamer@gmail.com>
2016-05-10 21:48:13 +0300

    Initial commit

---
+++ modules/cconverter/application/cconverter.cpp
@@ -0,0 +1,17 @@
+// Copyright 2016 Rumyantsev Alexander
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#include "include/currency_converter.h"
+
+int main(int argc, const char** argv) {
+  //CurrencyConverter app;
+  //std::string output = app(argc, argv);
+  //printf("%s\n", output.c_str());
+  
+  printf("Hello World!\n");
+
+  return 0;
+}

9d67bb8e1aacf3c3e065da0dc44fa4eb99ccee76
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2016-05-15 20:26:44 +0400

    fix constructtor name

--- modules/roman-number/include/RomanConverter.h
+++ modules/roman-number/include/RomanConverter.h
@@ -6,7 +6,7 @@
 
 class RomanConverter {
  public:
-    RomnanConverter() = delete;
+    RomanConverter() = delete;
     static bool CheckArabicNumber(int arabic);
     static bool CheckRomanNumber(const std::string& roman);
     static int ConvertRomanToArabic(const std::string& roman);

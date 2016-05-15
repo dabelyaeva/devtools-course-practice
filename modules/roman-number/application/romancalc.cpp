5dee20aeae0c59ce87833ffcb3db45be42266558
DolotovEvgeniy <dolotov.evgeniy@gmail.com>
2016-05-15 17:45:30 +0400

    output application result in stream

--- modules/roman-number/application/romancalc.cpp
+++ modules/roman-number/application/romancalc.cpp
@@ -1,15 +1,13 @@
 // Copyright 2016 Dolotov Evgeniy
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string>
+#include <iostream>
 
 #include "include/roman_calculator.h"
 
 int main(int argc, const char** argv) {
     RomanCalculator app;
-    std::string output = app(argc, argv);
-    printf("%s\n", output.c_str());
+
+    std::cout << app(argc, argv) << std::endl;
 
     return 0;
 }

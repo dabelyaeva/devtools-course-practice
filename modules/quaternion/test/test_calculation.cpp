d9c56585f691b39db917b4b79f99807ae7d03bf2
Asya <6991as@mail.ru>
2016-05-20 18:21:32 +0400

    new tests error 5

--- modules/quaternion/test/test_calculation.cpp
+++ modules/quaternion/test/test_calculation.cpp
@@ -1,13 +1,12 @@
 // Copyright 2016 Arhipycheva Anastasia
 
 #include "gtest/gtest.h"
-#include "include/quaternion_calculator.h"
+#include "src/quaternion_calculator.cxx"
 
 TEST(quaternion_calculation, parseOperation_1) {
   // Arrange
   const char* arg = "+";
-  QuaternionCalculator QC;
-  char result = QC.parseOperation(arg);
+  char result = parseOperation(arg);
   char exp = '+';
   // Act && Assert
   EXPECT_EQ(exp, result);
@@ -16,80 +15,71 @@ TEST(quaternion_calculation, parseOperation_1) {
 TEST(quaternion_calculation, parseOperation_2) {
   // Arrange
   const char* arg = "-";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('-', QC.parseOperation(arg));
+  EXPECT_EQ('-', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_3) {
   // Arrange
   const char* arg = "*";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('*', QC.parseOperation(arg));
+  EXPECT_EQ('*', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_4) {
   // Arrange
   const char* arg = "!";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('!', QC.parseOperation(arg));
+  EXPECT_EQ('!', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_5) {
   // Arrange
   const char* arg = "*";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('*', QC.parseOperation(arg));
+  EXPECT_EQ('*', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_6) {
   // Arrange
   const char* arg = "~";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('~', QC.parseOperation(arg));
+  EXPECT_EQ('~', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_M) {
   // Arrange
   const char* arg = "M";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('M', QC.parseOperation(arg));
+  EXPECT_EQ('M', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_Q) {
   // Arrange
   const char* arg = "Q";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('Q', QC.parseOperation(arg));
+  EXPECT_EQ('Q', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseOperation_Q) {
   // Arrange
   const char* arg = "N";
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ('N', QC.parseOperation(arg));
+  EXPECT_EQ('N', parseOperation(arg));
 }
 
 TEST(quaternion_calculation, parseDouble) {
   // Arrange
   const char* arg; = '9'
-  QuaternionCalculator QC;
 
   // Act && Assert
-  EXPECT_EQ(9, QC.parseDouble(arg));
+  EXPECT_EQ(9, parseDouble(arg));
 }

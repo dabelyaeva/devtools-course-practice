b9b55669fb53ce4dd87845e69e1860d54cb2c292
Georgy <georg3796@gmail.com>
2016-05-20 23:48:09 +0300

    travis fix

--- modules/path/include/AppPath.h
+++ modules/path/include/AppPath.h
@@ -1,17 +1,17 @@
 // Copyright 2016 Orlov Georgy
 
-#ifndef MODULES_PATH_INCLUDE_APPLICATION_H_
-#define MODULES_PATH_INCLUDE_APPLICATION_H_
+#ifndef MODULES_PATH_INCLUDE_APPPATH_H_
+#define MODULES_PATH_INCLUDE_APPPATH_H_
 
 #include <string>
 #include "include/PathFinder.h"
 
 class AppPath {
-public:
+ public:
     AppPath();
     std::string operator()(int argc, const char** argv);
 
-private:
+ private:
     void help(const char* appname, const char* message = "");
     int** readFile(std::string filename);
     int getGraphSizeFromFile(std::string filename);
@@ -30,4 +30,4 @@ private:
     PathFinder pf;
 };
 
-#endif  // MODULES_PATH_INCLUDE_APPLICATION_H_
+#endif  // MODULES_PATH_INCLUDE_APPPATH_H_

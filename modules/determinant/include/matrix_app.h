02638ad004677603c8b112919ee07c8e26201903
Kulish Simon <sem_kulish@mail.ru>
2016-05-20 16:59:59 +0400

    delete extra spaces

--- modules/determinant/include/matrix_app.h
+++ modules/determinant/include/matrix_app.h
@@ -5,28 +5,27 @@
 
 #include <string>
 #include <vector>
-#include "matrix.h"
+#include "./matrix.h"
 
 using std::vector;
 
-
 class MatrixApplication {
  public:
-   MatrixApplication();
-   std::string operator()(int argc, const char** argv);
-   bool validateNumberOfArguments(int argc, const char** argv);
+  MatrixApplication();
+  std::string operator()(int argc, const char** argv);
+  bool validateNumberOfArguments(int argc, const char** argv);
  private:
-   void help(const char* arg, const char* message = "");
-   std::string message_;
-   int parseToInt(const char* arg);
+  void help(const char* arg, const char* message = "");
+  std::string message_;
+  int parseToInt(const char* arg);
 
-   typedef struct {
-     int _size;
-     vector<int> _elems;
-     std::string _act;
-     int _row = 0;
-     int _collum = 0 ;
-   } Arguments;
+  typedef struct {
+   int _size;
+   vector<int> _elems;
+   std::string _act;
+   int _row = 0;
+   nt _collum = 0 ;
+  } Arguments;
 };
 
-#endif // MODULES_DETERMINANT_INCLUDE_MATRIX_APP_H_
+#endif  // MODULES_DETERMINANT_INCLUDE_MATRIX_APP_H_

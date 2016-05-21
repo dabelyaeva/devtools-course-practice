6b0a6ff8c935b271ed077057e990f3d9667a3c26
sharad_brat <sharoobrazno@mail.ru>
2016-05-21 12:37:52 +0300

    fixing style

--- modules/binarytree/include/binary_tree_app.h
+++ modules/binarytree/include/binary_tree_app.h
@@ -1,17 +1,16 @@
 // Copyright 2016 Sharadze Georgy
 
-// Copyright 2016 Sharadze Georgy
-#ifndef MODULES_INCLUDE_BINARY_TREE_APP_H_
-#define MODULES_INCLUDE_BINARY_TREE_APP_H_
+#ifndef MODULES_BINARYTREE_INCLUDE_BINARY_TREE_APP_H_
+#define MODULES_BINARYTREE_INCLUDE_BINARY_TREE_APP_H_
 
 #include <string>
 
 class BinaryTreeApplication {
-public:
+ public:
     BinaryTreeApplication();
     std::string operator()(int argc, const char** argv);
 
-private:
+ private:
     void help(const char* appname, const char* message = "");
     bool validateNumberOfArguments(int argc, const char** argv);
     std::string message_;
@@ -22,4 +21,4 @@ private:
     } Arguments;
 };
 
-#endif  // MODULES_INCLUDE_BINARY_TREE_APP_H_
+#endif  // MODULES_BINARYTREE_INCLUDE_BINARY_TREE_APP_H_

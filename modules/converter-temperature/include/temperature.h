e0a37944e39ad28bd9841016e68d7a0a1786103c
RyabininK4 <RyabininK4@users.noreply.github.com>
2016-05-21 13:54:12 +0300

    Update temperature.h

--- modules/converter-temperature/include/temperature.h
+++ modules/converter-temperature/include/temperature.h
@@ -18,7 +18,7 @@ class TemperConverter {
     Measure getMeasure(const std::string& measure) const;
     std::string message_;
     typedef struct {
-        std::string value_con;
+        double value_con;
         std::string unit_con;
         std::string unit_res;
      } Arguments;

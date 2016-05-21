87a4e18ffdcce318261aa1cf66110dc122868945
Lenferd <DeysonSH@gmail.com>
2016-05-21 20:46:56 +0300

    added new files. travis fix

--- modules/huffman_alg/application/huffman_coding.cpp
+++ modules/huffman_alg/application/huffman_coding.cpp
@@ -4,6 +4,6 @@
 #include "include/huffman_alg.h"
 
 int main(int argc, const char** argv) {
-	std::cout << "This work" << std::endl;
+  std::cout << "This work" << std::endl;
   return 0;
 }
---
+++ modules/huffman_alg/test/test_huffman_coding.cpp
@@ -0,0 +1,9 @@
+// Copyright 2016 Sergey Shumihin
+
+#include <gtest/gtest.h>
+#include "include/huffman_coding.h"
+
+TEST(HuffmanCoding, just_checking)
+{
+  EXPECT_EQ(2, 2);
+}

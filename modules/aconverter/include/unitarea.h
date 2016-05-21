f26bc16b69359104b92d6fa21aa44848f60bde39
Andrew-Voevodin <andrvoevodin@mail.ru>
2016-05-21 23:31:28 +0300

    TR19

--- modules/aconverter/include/unitarea.h
+++ modules/aconverter/include/unitarea.h
@@ -9,6 +9,16 @@
 using std::string;
 using std::vector;
 
+const AreaUnit AREA_UNIT_METER = AreaUnit(0.01, "m");
+const AreaUnit AREA_UNIT_WEAVING = AreaUnit(1, "ar");
+const AreaUnit AREA_UNIT_HECTARE = AreaUnit(100, "g");
+
+const vector<AreaUnit> DefaultAreaUnits = {
+      AREA_UNIT_METER,
+      AREA_UNIT_WEAVING,
+      AREA_UNIT_HECTARE
+};
+
 class AreaUnit {
  public:
     AreaUnit(double coefficient, const string);

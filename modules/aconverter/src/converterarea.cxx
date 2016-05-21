89ad02d43bf59661e092fbf918c5d20b004a677f
Andrew-Voevodin <andrvoevodin@mail.ru>
2016-05-21 23:58:36 +0300

    TR24

--- modules/aconverter/src/converterarea.cxx
+++ modules/aconverter/src/converterarea.cxx
@@ -13,11 +13,11 @@
 using std::vector;
 using std::string;
 
-const AreaUnit AREA_UNIT_METER = AreaUnit(0.01, "m");
-const AreaUnit AREA_UNIT_WEAVING = AreaUnit(1, "ar");
-const AreaUnit AREA_UNIT_HECTARE = AreaUnit(100, "g");
+static const AreaUnit AREA_UNIT_METER = AreaUnit(0.01, "m");
+static const AreaUnit AREA_UNIT_WEAVING = AreaUnit(1, "ar");
+static const AreaUnit AREA_UNIT_HECTARE = AreaUnit(100, "g");
 
-const vector<AreaUnit> DefaultAreaUnits = {
+static const vector<AreaUnit> DefaultAreaUnits = {
       AREA_UNIT_METER,
       AREA_UNIT_WEAVING,
       AREA_UNIT_HECTARE

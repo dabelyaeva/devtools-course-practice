8b40b7db5e77e85d0fce02bc54c822fb41a18e2d
Lenferd <DeysonSH@gmail.com>
2016-05-22 15:50:03 +0300

    style fix

--- modules/huffman_alg/src/huffman_coding.cxx
+++ modules/huffman_alg/src/huffman_coding.cxx
@@ -1,17 +1,16 @@
 // Copyright 2016 Sergey Shumihin
 
+#include <string>
 #include "include/huffman_coding.h"
 
 HuffmanCoding::HuffmanCoding() : _message("") {}
 
 void HuffmanCoding::help(const char * appname) {
-    _message = string("") + "This applicatiob work with Huffman coding\n" +
+    _message = std::string("") + "This applicatiob work with Huffman coding\n" +
         "To work with it specify what action should be performed in format:\n"
         + "# " + appname + " <operation> " + " <string> \n\n"
 
         + "<operator> must be one of:\n"
         + "-c \tIf you want to encode some string"
         + "-d \tIf you want to decode some code";
-
-
 }
